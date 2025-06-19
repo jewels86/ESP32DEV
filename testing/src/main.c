616b710319b44e6e4c30a3677387f63eb0566e1a
Jewels <jewels@jewels86.me>
2025-06-19 14:57:48 -0400

    Refactor pin utility functions to use integer pin numbers; update WiFi initialization for safer string handling

--- testing/src/main.c
+++ testing/src/main.c
@@ -1,5 +1,12 @@
 #include "hardware/abspins.h"
 
 void app_main() {
+    dPinIN(13);
+    dPinOUT(12);
+    while (true) {
+        int value = dRead(13);
+        if (value == HIGH) dWrite(12, HIGH);
+        else dWrite(12, LOW);
+    }
     
 }
